fda83b2d886394302e15efde5990be1e2052a234
Jasen Carroll <108644550+jasencarroll@users.noreply.github.com>
2023-03-14 11:19:53 -0700

    updated desploy

--- desploy.sh
+++ desploy.sh
@@ -28,3 +28,35 @@ sudo apt-get update
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start --driver=virtualbox
+
+## fixing swap
+#sudo swapoff -a
+#sudo dd if=/dev/zero of=/swapfile bs=1M count=32768 status=progress
+#sudo chmod 600 /swapfile
+#sudo mkswap /swapfile
+#sudo swapon /swapfile
+#echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+### Renegade - UUID=0bada02d-ac4e-462b-8a4e-b1d1dafddad7
+
+
+#free -h
+## Update /etc/deafult/grub with the following
+# blkid
+## GRUB_CMDLINE_DEFAULT add resume=UUID..... after queit splash
+## Add this to the bottom GRUB_RECORDFAIL_TIMEOUT=0
+
+# sudo filefrag -v /swapfile
+## GRUB_CMDLINE_LINUX_DEFAULT="quiet splash resume=UUID=0bada02d-ac4e-462b-8a4e-b1d1dafddad7 resume_offset=96946176"
+# sudo nano /etc/initramf-tools/conf.d/resume
+
+## sudo gedit /etc/polkit-1/localauthority/50-local.d/com.ubuntu.enable-hibernate.pkla
+## [Re-enable hibernate by default in upower]
+## Identity=unix-user:*
+## Action=org.freedesktop.upower.hibernate
+## ResultActive=yes
+##
+## [Re-enable hibernate by default in logind]
+## Identity=unix-user:*
+## Action=org.freedesktop.login1.hibernate;org.freedesktop.login1.handle-hibernate-key;org.freedesktop.login1;org.freedesktop.login1.hibernate-multiple-sessions;org.freedesktop.login1.hibernate-ignore-inhibit
+## ResultActive=yes
+# sudo apt install chrome-gnome-shell
